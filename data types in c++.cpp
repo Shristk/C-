f0c9de2a2473696853bc6e7ba164b0428cf4970b
Shrist Kumar <133365072+Shristk@users.noreply.github.com>
2024-08-09 10:06:39 +0530

    Add files via upload

---
+++ data types in c++.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"size of integer is "<<sizeof(int)<<" bytes\n";
+    cout<<"size of character is "<<sizeof(char)<<" bytes\n";
+    cout<<"size of float is "<<sizeof(float)<<" bytes\n";
+    cout<<"size of double is "<<sizeof(double)<<" bytes\n";
+    cout<<"size of boolean is "<<sizeof(bool)<<" bytes\n";
+    return 0;
+}
