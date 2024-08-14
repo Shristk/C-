b082cf392f2d93bfaab3314da48087d8b5cb6af3
Shrist Kumar <133365072+Shristk@users.noreply.github.com>
2024-08-14 12:05:06 +0530

    Add files via upload

---
+++ perfect_series.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class input{
+    public:
+    int n;
+    void input_number()
+    {
+        cout<<"enter the number"<<endl;
+        cin>>n;
+    }
+};
+
+class perfect{
+    public:
+    input obj1;
+    int sum(int i)
+    {
+        int s=0;
+        for(int j=1;j<i;j++)
+        {
+            if(i%j==0)
+            {
+                s=s+j;
+            }
+        }
+        return s;
+    }
+};
+int main(){
+    input obj1;
+    perfect obj2;
+    int start,end,s1;
+    cout<<"enter start and end values"<<endl;
+    cin>>start>>end;
+    for(int i=start;i<=end;i++) 
+    {
+        int t=i;
+       s1=obj2.sum(i);
+    if(s1==t)
+    {
+        cout<<t<<endl;
+    }
+    }
+}
