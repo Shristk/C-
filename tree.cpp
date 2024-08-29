df556261063d4b17e405387f6f07cf48fe12a576
Shrist Kumar <133365072+Shristk@users.noreply.github.com>
2024-08-29 10:09:21 +0530

    tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -28,3 +28,53 @@ int main()
     root->left->right=new treenode(50);
     inorder(root);
 }
+//==============================================================================================================================
+//NODE INSERTION IN BINARY TREE
+#include<iostream>
+using namespace std;
+#include<queue>
+class treenode{
+    public:
+    int data;
+    treenode* left;
+    treenode* right;
+    treenode(int value){
+        data = value;
+        left=NULL;
+        right=NULL;
+    }
+};
+void inorder(treenode* root){
+    if(root!=NULL)
+    {
+        inorder(root->left);
+    }
+}
+int main()
+{
+    treenode* root;
+    int x,y,z;
+    cout<<"enter the root data: ";
+    cin>>x;
+    root= new treenode(x);
+    queue<treenode*>q;
+    q.push(root);
+    while(!q.empty())
+    {
+        treenode* temp=q.front();
+        q.pop();
+        cout<<"enter the left data: ";
+        cin>>y;
+        if(y!=-1)
+        {
+            temp->left=new treenode(y);
+            q.push(temp->left);
+        }
+        cout<<"enter the right data: ";
+        if(z!=-1)
+        {
+            temp->right=new treenode(z);
+            q.push(temp->right);
+        }        
+    }
+}
