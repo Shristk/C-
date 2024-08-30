bff6d48b75ec56815f6395ef8d500ec16c7020a2
Shrist Kumar <133365072+Shristk@users.noreply.github.com>
2024-08-30 14:05:13 +0530

    Add files via upload

---
+++ fibo.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int fibonacci(int n){
+    if(n<1)
+    {
+        return 1;
+    }
+    else{
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
+int main()
+{
+    int n;
+    cout<<"enter the range of the fibonacci series"<<endl;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cout<<fibonacci(i)<<" ";
+    }
+}
